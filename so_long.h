bb60860b9190c78a1fe1254b045d14d2b639edf5
Alain Sene <zakab.66@gmail.com>
2024-11-20 15:56:21 +0100

    maps

--- so_long.h
+++ so_long.h
@@ -1,3 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 15:23:00 by asene             #+#    #+#             */
+/*   Updated: 2024/11/20 15:55:49 by asene            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/libft.h"
-#include <stdio.h> 
+#include <stdio.h>
 #include <fcntl.h>
+
+typedef struct s_map
+{
+	unsigned int	width;
+	unsigned int	height;
+}	t_map;
